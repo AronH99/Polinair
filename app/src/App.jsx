20c845ec081d7ff20e607fa64301d46388337026
AronH99 <aronheremans@gmail.com>
2022-04-04 16:36:30 +0200

    radioboxcomponent gemaakt + refactoring axios hooks + some debugging

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,65 +1,58 @@
-import { useState, useEffect } from "react";
-import React from "react";
-import axios from "axios";
+import { useState, useEffect, React } from "react";
+import useAxios from "axios-hooks";
 import CityRequest from "./components/CityLocationRequest/CityLocationRequest";
 import Navbar from "./components/Navbar/Navbar";
 import Form from "./components/Interface/Interface";
 import Body from "./components/Body/Body";
-import Location from "./components/Geolocation/GeoLocation";
+import GeoLocation from "./components/Geolocation/GeoLocation";
+import RadioButton from "./components/RadioButton/RadioButton";
 import "./style.scss";
 
 const apiuitbreiding =
   "https://api.breezometer.com/pollen/v2/forecast/daily?lat=48.857456&lon=2.354611&days=3&key=8496f755e9fb4717970612a504b952f3&features=types_information,plants_information";
 
 const App = () => {
-  const [standarddata, setStandardData] = useState({ data: [] });
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState(1);
+  const [days, setDays] = useState(1);
   const [lat, setLat] = useState();
   const [lon, setLon] = useState();
-  const [lat2, setLat2] = useState();
-  const [lon2, setLon2] = useState();
-  const [chosenCity, setChosenCity] = useState();
+  const [input, setInput] = useState("");
+  const [locationbool, setLocationbool] = useState(true);
+
+  const [{ data: standarddata, loading, error }, fetchBreezoData] = useAxios(
+    `https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lon}&key=8496f755e9fb4717970612a504b952f3&days=${days}`,
+    { manual: true }
+  );
 
   useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true);
-        setError(false);
-        const { data } = await axios(
-          `https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat2}&lon=${lon2}&key=8496f755e9fb4717970612a504b952f3&days=${input}`
-        );
-        setLoading(false);
-        setError(false);
-        setStandardData(data);
-      } catch (error) {
-        setError(true);
-        setLoading(false);
-        setStandardData({ data: [] });
-      }
-    })();
-  }, [input, lat2, lon2]);
+    if (days && lat && lon) {
+      fetchBreezoData();
+    }
+  }, [days, lat, lon]);
 
   return (
     <>
       <Navbar />
-      <Form setInput={setInput}>
-        <CityRequest
-          setLat2={setLat2}
-          setLon2={setLon2}
-          setChosenCity={setChosenCity}
-          chosenCity={chosenCity}
-          lat={lat}
-          lon={lon}
-        />
+      <RadioButton
+        setLocationbool={setLocationbool}
+        locationbool={locationbool}
+        setInput={setInput}
+      />
+      <Form setDays={setDays}>
+        {!locationbool && (
+          <CityRequest
+            setLat={setLat}
+            setLon={setLon}
+            setInput={setInput}
+            input={input}
+          />
+        )}
       </Form>
-      <Location setLat={setLat} setLon={setLon} setError={setError} />
+      {locationbool && <GeoLocation setLat={setLat} setLon={setLon} />}
       <Body
-        standarddata={standarddata}
         error={error}
         loading={loading}
-        chosenCity={chosenCity}
+        standarddata={standarddata}
+        input={input}
       />
     </>
   );
